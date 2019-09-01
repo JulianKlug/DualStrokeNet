94bcac924554fb0d31b6ab1d2b283594675bab2e
monsieurwave <tensu.wave@gmail.com>
2019-09-01 21:54:56 +0200

    Use gridsearch to establish effect of parameters training/testing

---
+++ gridsearch.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+
+epochs = 30
+modality = 'mri'
+
+for network_size in range(1, 128, 12):
+    for batch_size in 2.56 * 10**np.random.uniform(1, 10, size=4):
+        batch_size = int(batch_size)
+        for lr in 10**np.random.uniform(-5, 1, size=4):
+            os.system(f'python train_{modality}.py'
+                      f' -d /home/klug/data/working_data/withDWI_all_2016_2017/standardized_data_set.npz '
+                      f'--two-d -e {epochs} -c {network_size} -b {batch_size} --lr-1 {lr} '
+                      f'> {modality}_gridsearch_logs/{network_size}_{batch_size}_{lr}.log')
+
